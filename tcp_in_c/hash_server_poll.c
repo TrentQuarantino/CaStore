788546871136622c491f0ab0e6cd20850d67e27b
Mauro Bertozzi <teotozzi@gmail.com>
2016-07-19 15:11:31 +0200

    add server

--- tcp_in_c/hash_server_poll.c
+++ tcp_in_c/hash_server_poll.c
@@ -14,10 +14,7 @@
 
 #define MAX(a, b)       ((a) > (b) ? (a) : (b))
 
-struct pollfd fds[50];
-int srv_fd, cfd;
-int fd_count;
-int yep = 1;
+typedef int (*client_func_t)(int cfd);
 
 struct node {
     struct node *next;
@@ -29,6 +26,89 @@ struct hashtable {
     struct node **heads;
     int vect_size;
 };
+
+int create_server (int port) {
+    int srv_fd;
+    int yep = 1;
+    struct sockaddr_in servaddr;
+    srv_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    setsockopt(srv_fd, SOL_SOCKET, SO_REUSEADDR, &yep, sizeof(int));
+
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htons(INADDR_ANY);
+    servaddr.sin_port = htons(port);
+
+    bind(srv_fd, (struct sockaddr *) &servaddr, sizeof(servaddr));
+    listen(srv_fd, 10);
+    return(srv_fd);
+}
+
+void server_loop (int srv_fd, client_func_t c_func) {
+    struct pollfd fds[50];
+    int cfd;
+    int fd_count;
+    fd_count = 1;
+    fds[0].fd = srv_fd;
+    fds[0].events = POLLIN;
+
+    while (1) {
+        printf("WAIT ON SELECT %d\n", fd_count);
+        if (poll(fds, fd_count, -1) < 0) {
+            perror("poll()");
+            continue;
+        }
+
+        // qualcuno si e' connesso
+        if (fds[0].revents & POLLIN) {
+            int cfd = accept(srv_fd, (struct sockaddr*) NULL, NULL);
+            int yep = 1;
+            setsockopt(cfd, IPPROTO_TCP, TCP_NODELAY, &yep, sizeof(int));
+            printf("Accettato il client %d\n", cfd);
+            fds[fd_count].fd = cfd;
+            fds[fd_count].events = POLLIN;
+            fd_count++;
+        } else {
+
+            printf("SCAN FOR READ fda=%d\n", fd_count);
+            for (int i = 1; i < fd_count; ++i) {
+                int cfd = fds[i].fd;
+                if (!(fds[i].revents & POLLIN)) continue;
+
+                if (c_func(cfd) < 0) {
+                    memmove(fds + i, fds +i +1, (fd_count - i) * sizeof(struct pollfd));
+                    fd_count--;
+                    continue;
+                }
+            }
+        }
+    }
+}
+
+int client_echo (int cfd) {
+    char buffer[128];
+    ssize_t = rd;
+    //leggiamo quello che ha scritto un client
+    rd = read(cfd, buffer, sizeof(buffer));
+    if (rd <= 0) {
+        return(-1);
+    }
+    if (mecmp(buffer, "scus", 4) == 0) {
+        memcpy(buffer, "figur\n", 6);
+        rd = 6;
+    } else if (memcmp(buffer, "chiudi", 6) == 0) {
+        return(-1);
+    } else if (memcmp(buffer, "put", 3) == 0) {
+        //todo
+    } else if (memcmp(buffer, "get", 3) == 0) {
+        //todo
+    }
+    printf(" - letto dal client %d: ", cfd);
+    stampa(buffer, rd);
+    write(cfd, buffer, rd);
+    return(0);
+}
 
 void stampa (const char *dato, int len) {
   int i;
@@ -65,70 +145,14 @@ char *hashtable_get (struct hashtable *self, int key) {
     return("non trovato");
 }
 
-void create_server () {
-    struct sockaddr_in servaddr;
-    srv_fd = socket(AF_INET, SOCK_STREAM, 0);
 
-    setsockopt(srv_fd, SOL_SOCKET, SO_REUSEADDR, &yep, sizeof(int));
 
-    bzero(&servaddr, sizeof(servaddr));
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = htons(INADDR_ANY);
-    servaddr.sin_port = htons(11124);
 
-    bind(srv_fd, (struct sockaddr *) &servaddr, sizeof(servaddr));
-    listen(srv_fd, 10);
-}
-
-void client_loop () {
-    fd_count = 1;
-    fds[0].fd = srv_fd;
-    fds[0].events = POLLIN;
-
-    while (1) {
-        printf("WAIT ON SELECT %d\n", fd_count);
-        if (poll(fds, fd_count, -1) < 0) {
-            perror("poll()");
-            continue;
-        }
-
-        // qualcuno si e' connesso
-        if (fds[0].revents & POLLIN) {
-            int cfd = accept(srv_fd, (struct sockaddr*) NULL, NULL);
-            setsockopt(cfd, IPPROTO_TCP, TCP_NODELAY, &yep, sizeof(int));
-            printf("Accettato il client %d\n", cfd);
-            fds[fd_count].fd = cfd;
-            fds[fd_count].events = POLLIN;
-            fd_count++;
-        } else {
-            char buffer[128];
-            ssize_t rd;
-            printf("SCAN FOR READ fda=%d\n", fd_count);
-            for (int i = 1; i < fd_count; ++i) {
-                int cfd = fds[i].fd;
-                if (!(fds[i].revents & POLLIN)) continue;
-
-                // leggiamo quello che ha scritto un client
-                rd = read(cfd, buffer, sizeof(buffer));
-                if (rd <= 0) {
-                    memmove(fds + i, fds +i +1, (fd_count - i) * sizeof(struct pollfd));
-                    fd_count--;
-                    continue;
-                }
-                printf(" - letto dal client %d: ", cfd);
-                stampa(buffer, rd);
-                /*memcpy(buffer + rd, "ciao\n",5);
-                printf("il buffer %s", buffer);
-                rd += 5;*/
-                write(cfd, buffer, rd);
-            }
-        }
-    }
-}
 
 int main (int argc, char **argv) {
-    create_server();
-    client_loop();
+    int srv_fd;
+    srv_fd = create_server(11124);
+    server_loop(srv_fd, client_echo);
 
     close(srv_fd);
     return(0);
