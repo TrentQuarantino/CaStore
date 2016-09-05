4ade6d29222d0029415119e16530f39d9246917d
Mauro Bertozzi <teotozzi@gmail.com>
2016-09-05 17:52:17 +0200

    aka pehora.c

---
+++ tcp_in_c/readerFile.c
@@ -0,0 +1,34 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#define MAX_BUF 128
+
+int readfile_init ( const char *filename) {
+    int fd;
+    fd = open(filename, O_RDONLY);
+    if (fd < 0) perror("open");
+    return(fd);
+}
+
+void readonly (int fd) {
+    char html[128];
+    int letten;
+    int tot = 0;
+    while ((letten = read(fd, html, 128)) > 0) {
+        tot += letten;
+        html[letten] = '\0';
+        write(1, html, letten);
+    }
+    printf("tot %d\n", tot);
+}
+
+int main(int argc, char **argv) {
+    int fd;
+    fd = readfile_init("pag_html");
+    readonly(fd);
+    close(fd);
+}
